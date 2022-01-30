b9a4d92d0d6882ab461bc26ea326613f9fd0e1a4
Xalo Dev <xalodeveloper@gmail.com>
2022-01-30 17:36:23 -0300

    Exception handling - delete

--- src/main/java/com/xalodev/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/xalodev/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.xalodev.course.resources.exceptions;
 
+import com.xalodev.course.services.exceptions.DataBaseException;
 import com.xalodev.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,4 +21,14 @@ public class ResourceExceptionHandler {
                 new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError> databaseError(DataBaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err =
+                new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
 }
--- src/main/java/com/xalodev/course/services/UserService.java
+++ src/main/java/com/xalodev/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.xalodev.course.services;
 
 import com.xalodev.course.entities.User;
 import com.xalodev.course.repositories.UserRepository;
+import com.xalodev.course.services.exceptions.DataBaseException;
 import com.xalodev.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,14 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try{
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataBaseException(e.getMessage());
+        }
+
     }
 
     public User update(Long id, User obj) {
